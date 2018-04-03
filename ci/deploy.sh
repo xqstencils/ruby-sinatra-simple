e9ba2078d0bf586391d965117224fd043a065ccb
Cao Xiaoqi <xqcao@thoughtworks.com>
2018-04-03 13:52:29 +0800

    Fix

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# heroku git:remote -a $HEROKU_APP_NAME
 # git push --force git@heroku.com:$HEROKU_APP_NAME.git HEAD:refs/heads/master
-git push https://heroku:$HEROKU_API_KEY@git.heroku.com/$HEROKU_APP_NAME.git HEAD:refs/heads/master
+heroku git:remote -a $HEROKU_APP_NAME
+git push --force https://heroku:$HEROKU_API_KEY@git.heroku.com/$HEROKU_APP_NAME.git HEAD:refs/heads/master
 sleep 5
 heroku restart
